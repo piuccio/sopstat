915b87edf1957995e39294fe71447e081d950566
mana.matteo <mana.matteo@bb977290-0bf0-11de-8884-7b8501395459>
2009-03-17 20:45:46 +0000

    Rebuild list structure... - 4 pointers for each IP (upTCP dwTCP dwUDP upTCP) - print directly in 4 different files - separate each host by #<ip_address> - a lot of dirty code to clean...

--- trunk/sopstat/src/liste.h
+++ trunk/sopstat/src/liste.h
@@ -8,14 +8,21 @@
 #include "packet.h"
 
 typedef struct ipnode {
-	u_long ip;
+   u_long ip;
    char  address [MAX_IP_ADDR];
-   struct packet_stat* first;
+   struct packet_stat* upTCP;
+   struct packet_stat* dwTCP;
+   struct packet_stat* upUDP;
+   struct packet_stat* dwUDP;
    struct ipnode* next;    	
 } ipnode;
 
 void insert_stat(packet_stat *, packet_stat *);
-void insert_node(ipnode* , u_int, packet_stat *);
-void print(ipnode* );
+void insert_node(ipnode* , u_int, packet_stat *, direction);
+int print(ipnode*, char * );
+void printdwTCP(ipnode* );
+void printupTCP(ipnode* );
+void printdwUDP(ipnode* );
+void printupUDP(ipnode* );
 
 #endif /*LISTE_H_*/
